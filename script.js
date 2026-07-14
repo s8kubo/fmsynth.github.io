ba9564507fb9e1f5ad790d65f4045bc1ae52b2ba
大久保颯 <allaboutlilychouchouchouchou@gmail.com>
2026-07-14 22:02:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,61 @@ function createSynth(){
     mod.start();
     lfo.start();
 }
+
+function updateValues(){
+
+    let freq=parseFloat(freqSlider.value);
+    let ratio=parseFloat(ratioSlider.value);
+    let index=parseFloat(indexSlider.value);
+    let lfoRate=parseFloat(lfoSlider.value);
+
+    carrier.frequency.value=freq;
+
+    mod.frequency.value=freq*ratio;
+
+    // Index
+    modGain.gain.value=index*100;
+
+    // Vibrato
+    lfo.frequency.value=lfoRate;
+    lfoGain.gain.value=5;
+
+}
+
+const freqSlider=document.getElementById("freq");
+const ratioSlider=document.getElementById("ratio");
+const indexSlider=document.getElementById("index");
+const lfoSlider=document.getElementById("lfo");
+
+freqSlider.oninput=updateValues;
+ratioSlider.oninput=updateValues;
+indexSlider.oninput=updateValues;
+lfoSlider.oninput=updateValues;
+
+document.getElementById("play").onclick=()=>{
+
+    if(!playing){
+
+        createSynth();
+
+        playing=true;
+
+    }
+
+}
+
+document.getElementById("stop").onclick=()=>{
+
+    if(playing){
+
+        carrier.stop();
+        mod.stop();
+        lfo.stop();
+
+        audio.close();
+
+        playing=false;
+
+    }
+
+}
